72ad9eef77f65ee2d35ab6089f36d1c08ea9be86
Marko Sahlman <marko.sahlman@gmail.com>
2017-01-09 20:36:19 +0200

    Added last modified argument. Ie. Use -m 6 to include files that have been modified duriung last 6 hours

--- README.md
+++ README.md
@@ -9,9 +9,22 @@
 # - mutagen library (pip install mutagen)
 #
 # Usage:
-# python artist2albumartist -d /music/mp3/
+# python artist2albumartist [-r] [-m 24] -d /music/mp3/ 
 #
-# Version 1.0
+# -r recursive
+# -d directory
+# -m last modified in hours. ie if you want to scan files added in few hours you can use -m 6 for 6 hours files modified before it will be skipped
+#
+#
+#
+# Version 1.2
+# - Added last modified argument
+#
+# Version 1.1
+# - Added recursive option
+# Version 1.0¨
+# - Initial version
+#
 #
 # 24/11/2016 Marko Sahlman (marko.sahlman@gmail.com)
 #
--- artist2albumartist.py
+++ artist2albumartist.py
@@ -4,14 +4,7 @@
 # Scans given directory for mp3 and m4a files. Copies artist tag to album artist.
 # Why: Some Digital DJ softwares use album artist tag for tracks instead of artist and it makes things complicated.
 #
-# Requirements:
-# - Python
-# - mutagen library (pip install mutagen)
-#
-# Usage:
-# python artist2albumartist -d /music/mp3/
-#
-# Version 1.0
+# See README.md
 #
 # 24/11/2016 Marko Sahlman (marko.sahlman@gmail.com)
 #
@@ -21,16 +14,27 @@ from os.path import isfile, join
 import mutagen
 import sys, getopt, os
 import fnmatch
+import time
 
 def printUsageAndExit():
-    print 'Usage: [-r] -d <dir>'
+    print 'Usage: [-r] -d <dir> -m <last modified in hours>'
     sys.exit(1)
+
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
 
 def main(argv):
     dir_opt = False
     recursive_opt = False
+    last_modified = None
+    last_modified_in_ms = 0
+
     try:
-        opts,args = getopt.getopt(argv, "rd:")
+        opts,args = getopt.getopt(argv, "rd:m:")
     except getopt.GetoptError:
         printUsageAndExit()
 
@@ -40,6 +44,8 @@ def main(argv):
             dir_opt = True
         if opt == '-r':
             recursive_opt = True
+        if opt == '-m':
+            last_modified=arg
 
     if not dir_opt:
         printUsageAndExit()
@@ -51,6 +57,15 @@ def main(argv):
         print "Error: Directory not found"
         printUsageAndExit()
 
+    if last_modified != None:
+        if not is_number(last_modified):
+            print "Error: Last modified -m must be positive number"
+            printUsageAndExit()
+
+        last_modified_in_ms=3600*int(last_modified)
+
+    now = time.time()
+
     files = []
 
     if recursive_opt:
@@ -61,16 +76,30 @@ def main(argv):
         files = [f for f in os.listdir(directory) if isfile(join(directory,f)) ]
 
     for f in files:
+
         audiofile = join(directory,f)
+        audiofile_last_modified=os.path.getmtime(audiofile)
+
+        if last_modified != None:
+            if now - last_modified_in_ms > audiofile_last_modified:
+                continue
+
         if (audiofile.endswith(".mp3") or audiofile.endswith(".m4a")):
             try:
                 audio = mutagen.File(audiofile, easy=True)
-                artist=audio['artist'][0]
-                albumartist=audio['albumartist'][0]
-                if (albumartist != artist):
-                    print audiofile+": "+albumartist+"=>"+artist
+                artist=None
+                albumartist=None
+                if audio.get('artist'):
+                    artist=audio['artist'][0]
+                if audio.get('albumartist'):
+                    albumartist=audio['albumartist'][0]
+
+                if (artist != None and albumartist != artist):
+                    print audiofile+": "+str(albumartist)+"=>"+str(artist)
                     audio['albumartist']=artist
                     audio.save()
+                else:
+                    print audiofile+": Artist does not exist"
             except Exception as e:
                 print audiofile+": Failed to read/change file "
                 print "Error: "+str(e)
